c3c183095e2a82f8b209cc64c0596ce93d9a84ea
Prodigy <Prodigy@connecteo.mg>
2022-01-19 13:10:23 +0300

    read fait

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,11 @@
 # wsgi.py
 # pylint: disable=missing-docstring
-from flask import Flask, jsonify
+from asyncio.windows_events import NULL
+from flask import Flask, jsonify, abort
 
 app = Flask(__name__)
+
+data = [{"id":1,"name":"Skello"},{"id":2,"name":"Socialive.tv"},{"id":3,"name":"sup"}]
 
 @app.route('/')
 def hello():
@@ -10,4 +13,17 @@ def hello():
 
 @app.route('/api/v1/products')
 def produits():
-    return jsonify([{"id":1,"name":"Skello"},{"id":2,"name":"Socialive.tv"},{"id":3,"name":"sup"}])
+    return jsonify(data)
+
+@app.route('/api/v1/products/<id>')
+def produit_by_id(id):
+    result = ""
+    for var in data:
+        id_in_data=var["id"]
+        if id_in_data == int(id):
+            result = var
+            break
+    if result == "":
+        abort(404)
+    
+    return jsonify(result)
