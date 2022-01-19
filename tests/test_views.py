d795b17cac111adb68f8ad85f942678914a1b114
Prodigy <Prodigy@connecteo.mg>
2022-01-19 14:05:19 +0300

    test heroku fix

--- tests/test_views.py
+++ tests/test_views.py
@@ -13,13 +13,13 @@ class TestViews(TestCase):
         self.assertIsInstance(products, list)
         self.assertGreater(len(products), 2) # 2 is not a mistake here
     
-    def test_read_product_by_id_exists(self):
-        response = self.client.get("/api/v1/products/1")
-        product = response.json
-        self.assertEqual(response.status_code,200)
-        self.assertEqual(product["id"],1)
-        self.assertEqual(product["name"],"Skello")
+    # def test_read_product_by_id_exists(self):
+    #     response = self.client.get("/api/v1/products/1")
+    #     product = response.json
+    #     self.assertEqual(response.status_code,200)
+    #     self.assertEqual(product["id"],1)
+    #     self.assertEqual(product["name"],"Skello")
     
-    def test_read_product_by_id_not_exist(self):
-        response = self.client.get("/api/v1/products/6")
-        self.assertEqual(response.status_code,404)
+    # def test_read_product_by_id_not_exist(self):
+    #     response = self.client.get("/api/v1/products/6")
+    #     self.assertEqual(response.status_code,404)
--- wsgi.py
+++ wsgi.py
@@ -11,19 +11,19 @@ data = [{"id":1,"name":"Skello"},{"id":2,"name":"Socialive.tv"},{"id":3,"name":"
 def hello():
     return "Hello World! modif"
 
-@app.route('/api/v1/products')
-def produits():
-    return jsonify(data)
+# @app.route('/api/v1/products')
+# def produits():
+#     return jsonify(data)
 
-@app.route('/api/v1/products/<id>')
-def produit_by_id(id):
-    result = ""
-    for var in data:
-        id_in_data=var["id"]
-        if id_in_data == int(id):
-            result = var
-            break
-    if result == "":
-        abort(404)
+# @app.route('/api/v1/products/<id>')
+# def produit_by_id(id):
+#     result = ""
+#     for var in data:
+#         id_in_data=var["id"]
+#         if id_in_data == int(id):
+#             result = var
+#             break
+#     if result == "":
+#         abort(404)
     
-    return jsonify(result)
+#     return jsonify(result)
