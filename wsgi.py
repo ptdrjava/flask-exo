f6ca3a5880b0ba75952984577b29ec818b1e019d
Prodigy <Prodigy@connecteo.mg>
2022-01-19 12:18:47 +0300

    addin /api/V1/products url

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,13 @@
 # wsgi.py
 # pylint: disable=missing-docstring
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello World! modif"
+
+@app.route('/api/v1/produits')
+def produits():
+    return jsonify(PRODUITS = {1:{"id":1,"name":"Skello"},2:{"id":2,"name":"Socialive.tv"}})
